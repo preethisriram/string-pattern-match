7c9c8764dc6e60791c553457b913a2f416e0ba75
Preethi Sriram <srirampreethi@outlook.com>
2022-02-10 17:50:53 -0800

    Add project files.

---
+++ string pattern dropbox/Program.cs
@@ -0,0 +1,45 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+static bool patternMatch(String str, String pattern)
+{
+    
+    String[] splitstr = str.Split(" ");
+
+    char[] patternchar = pattern.ToArray();
+    HashSet<String> words = new HashSet<String>();
+
+    if (splitstr.Length != patternchar.Length)
+        return false;
+
+    Dictionary<char, string> pattmatch = new Dictionary<char, string>();
+
+    for(int i = 0;i<pattern.Length;i++)
+    {
+        if(pattmatch.ContainsKey(pattern[i]))
+        {
+            var currword = pattmatch[pattern[i]];
+            if (currword != splitstr[i])
+                return false;
+        }
+        else
+        {
+            if(words.Contains(splitstr[i]))
+                return false;
+            pattmatch.Add(pattern[i],splitstr[i]);
+            words.Add(splitstr[i]);
+        }
+    }
+
+    return true;
+
+
+
+
+}
+
+
+String str = "red blue green red red";
+String pattern = "abcaa";
+bool res = patternMatch(str, pattern);
+Console.WriteLine(res);
